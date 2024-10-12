1e80c5561a2df61179f7c79bd21443e19cda0f73
Maksym <maksimbondar225@gmail.com>
2024-10-12 14:55:08 +0200

    add telegram bot configuration

---
+++ src/main/java/com/bot/unrealfacegeneratorbot/bot/config/TelegramBotConfig.java
@@ -0,0 +1,31 @@
+package com.bot.unrealfacegeneratorbot.bot.config;
+
+import com.bot.unrealfacegeneratorbot.bot.TelegramBot;
+import lombok.SneakyThrows;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.telegram.telegrambots.bots.DefaultBotOptions;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
+
+@Configuration
+public class TelegramBotConfig {
+
+        @Bean
+        @SneakyThrows
+        public TelegramBot telegramBot(
+            @Value("${bot.token}") String botToken
+    ) {
+        DefaultBotOptions botOptions = new DefaultBotOptions();
+        TelegramBot bot = new TelegramBot(botOptions, botToken);
+        telegramBotsApi().registerBot(bot);
+        return bot;
+    }
+
+    @Bean
+    @SneakyThrows
+    public TelegramBotsApi telegramBotsApi() {
+        return new TelegramBotsApi(DefaultBotSession.class);
+    }
+}
