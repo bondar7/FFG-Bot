ead53c0ded07be01eae5c53188fef2af2d33b18c
Maksym <maksimbondar225@gmail.com>
2024-10-12 14:31:51 +0200

    first commit

---
+++ src/test/java/com/bot/unrealfacegeneratorbot/UnrealFaceGeneratorBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bot.unrealfacegeneratorbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnrealFaceGeneratorBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
